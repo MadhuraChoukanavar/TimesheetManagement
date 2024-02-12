f927ad6f277a765c6756323047592513be669f6e
KalyaniNagula <kalyanreddynagula@gmail.com>
2024-02-12 16:08:22 +0530

    first commit

---
+++ timesheet_entry_service/src/main/java/com/feuji/timesheetentryservice/repository/HolidayRepository.java
@@ -0,0 +1,9 @@
+package com.feuji.timesheetentryservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.feuji.timesheetentryservice.entity.HolidayEntity;
+
+public interface HolidayRepository extends JpaRepository<HolidayEntity, Long>{
+
+}
